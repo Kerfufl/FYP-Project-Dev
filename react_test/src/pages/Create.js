fd905e0e329eb8d60a55d9d0fda7b1012c7f7f7d
Kerfufl <eoinj@live.ie>
2023-02-08 12:36:38 +0000

    3D model texture test
    Added temp texture to modified box model to text textures

--- react_test/src/pages/Create.js
+++ react_test/src/pages/Create.js
@@ -1,35 +1,46 @@
-import React, {useRef} from 'react';
-import {Canvas, useFrame} from '@react-three/fiber'
+import React, {useRef,useState} from 'react';
+import {Canvas, useFrame, useLoader} from '@react-three/fiber'
+import { TextureLoader } from 'three';
+import { OrbitControls } from '@react-three/drei';
+
+
 export default function Edit() 
 {
     function Box(props)
     {
         const ref = useRef();
-
-        useFrame((state,delta) => (ref.current.rotation.x += delta))
+        const text = require("../img/In the Court of the Stone Defender.png")
+        const b =useLoader(TextureLoader,text)
+        
+        //useFrame((state,delta) => (ref.current.rotation.x += delta))
 
         return (
             <mesh
                 {...props}
                 ref={ref}
             >
-                <boxGeometry args={[1,1,1]}/>
-                <meshStandardMaterial color = 'hotpink' />
+                <boxGeometry args={[16,9,.01]}/>
+                {/* //<planeGeometry args={[16,9,1]}/> */}
+                <meshStandardMaterial map={b} />
              
             </mesh>
         )
     }
     return(
         <>
-        <h1>edit?</h1>
+        
+        
+        <div style = {{height:'90%',border:'2px solid black', justifyContent: 'center'}}>
         <Canvas>
                 <ambientLight/>
-                <pointLight />
-                <Box position = {[-1.2,0,0]}/>
-            </Canvas>
-        <div>
-            
+                {/* <pointLight /> */}
+                
+                <Box position = {[-1.2,0,-2]}/>
+                <OrbitControls />
+        </Canvas>
         </div>
+
+        
         </>
     );
 }
