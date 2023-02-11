6e0ab3b18a556159ab60fe1d0a674755914a7dc2
Kerfufl <c19382426@mytudublin.ie>
2023-02-11 15:07:13 +0000

    React component dynamics
    More testing with externally imported functions and the adding/removing of components

--- react_test/src/pages/Browse.js
+++ react_test/src/pages/Browse.js
@@ -1,6 +1,5 @@
-import React, {useRef, useState} from 'react';
-import {Canvas, useLoader} from '@react-three/fiber'
-import { TextureLoader } from 'three';
+import React, {useState} from 'react';
+import {Canvas} from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
 import '../index.css';
 import {Box} from './Create';
@@ -28,24 +27,39 @@ export default function Share()
     //         </mesh>
     //     )
     // }
-    const [divList] = useState([{comp: ''},{comp: ''},{comp: ''},{comp: ''}])
+    const [divList,setDivList] = useState([{comp: ''},{comp: ''},{comp: ''},{comp: ''}])
+
+    const addDiv = () => {
+        setDivList([...divList, {comp: ""}])
+}
+
+    const remDiv = (ind) => {
+        const list = [...divList]
+        list.pop()
+        setDivList(list)
+    }
+
     return(
         <>
         <div style={{display:'flex',justifyContent:'center', alignItems:'center', marginBottom: '5px'}}>
             <input type={"search"} style={{width:'60%',height:'20px'}}/>
-            <input type={"button"} value="Search models"/>
+            
+            <input type={"button"} value="Search models" onClick={addDiv}/>
             {/* <h1 style={{alignSelf:'center'}}>Browse test</h1> */}
         </div>
         <div class="flex-container" id="share" >
 			
             {divList.map((singleDiv, index) => (
             <div class="choice"  id="div1" >
-				<h1>Example</h1>
+				<h1>Example {index+1}
+                
+                <button type='button' onClick={() => remDiv(index)}
+                >Remove</button></h1>
+                
 				<h3>By: Anonymous</h3>
 				<h3>Date: 08.12.2022</h3><hr/>
                 <Canvas style={{width:300,height:260, margin:'auto', border:"2px black solid"}}>
                     <ambientLight/>
-                    {/* <pointLight /> */}
                     
                     <Box position = {[0,0,-6]} />
                     <OrbitControls />
