62e004bd97b6f20a3e38eec56674c05f3e0edf31
Kerfufl <eoinj@live.ie>
2023-03-30 21:41:28 +0100

    Failed test attempts
    Tried getting unit tests to happen, too many variables means none of the tests work for some reason, abandoned for now

---
+++ react_test/src/__tests__/render.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../index';
+
+
+test('renders learn react link', () => {
+  //const div = document.createElement('div');
+  render(
+    <App/>
+    );
+  
+});
+
